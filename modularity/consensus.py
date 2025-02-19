7f8f29855e6c2b8d294a37780d7db366e0a5fb76
Ankit Khambhati <akhambhati@gmail.com>
2025-02-18 21:35:28 -0800

    Include initial consensus estimate.

--- modularity/consensus.py
+++ modularity/consensus.py
@@ -7,7 +7,7 @@ louvain_helper = lambda x: genlouvain(x, limit=1000, verbose=False)
 
 
 def gen_consensus(B, n_consensus, max_tries=10, modularity_fn=None): 
-    n_tries = 0
+    A_cons_init = None 
     for n_tries in range(max_tries):
         A_cons = np.zeros_like(B)
         for n_iter in range(n_consensus):
@@ -16,11 +16,13 @@ def gen_consensus(B, n_consensus, max_tries=10, modularity_fn=None):
             else:
                 comms = modularity_fn(B)
             A_cons += comms_to_coassign(comms)
-        
+        if A_cons_init is None:
+            A_cons_init = A_cons
+
         n_uncertain = ((A_cons > 0) & (A_cons < n_iter)).sum()
         if n_uncertain == 0:
             break
 
         B = A_cons - A_cons[*np.triu_indices_from(A_cons, k=1)].mean()
 
-    return comms
+    return comms, A_cons_init
