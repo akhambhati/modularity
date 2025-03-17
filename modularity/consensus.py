fe800649859af1670b14686e577959746f82366d
Ankit Khambhati <akhambhati@gmail.com>
2025-03-17 11:26:39 -0700

    On-demand null model estimation for more dynamic consensus generation

--- modularity/consensus.py
+++ modularity/consensus.py
@@ -3,19 +3,36 @@ import numpy as np
 from .genlouvain import genlouvain 
 from .stats import comms_to_coassign 
 
+
 louvain_helper = lambda x: genlouvain(x, limit=1000, verbose=False)
 
 
-def gen_consensus(B, n_consensus, max_tries=10, modularity_fn=None): 
+def rewire_helper(A):
+    A_null = np.zeros_like(A)
+    A_null[*np_triu_indices_from(A, k=1)] = np.random.permutation(
+            A[*np.triu_indices_from(A, k=1)])
+    A_null += A_null.T
+    return A_null
+
+
+def gen_consensus(A, n_consensus, max_tries=10, P_estimator=None, modularity_fn=None): 
+    A = A.copy()
+
     A_cons_init = None 
     for n_tries in range(max_tries):
-        A_cons = np.zeros_like(B)
+        A_cons = np.zeros_like(A)
         for n_iter in range(n_consensus):
+            if P_estimator is None:
+                B = A - rewire_helper(A)
+            else:
+                B = A - P_estimator(A)
             if modularity_fn is None:
                 comms = louvain_helper(B)
             else:
                 comms = modularity_fn(B)
             A_cons += comms_to_coassign(comms)
+        A_cons[*np.diag_indices_from(A_cons)] = 0
+
         if A_cons_init is None:
             A_cons_init = A_cons / n_consensus
 
@@ -23,6 +40,6 @@ def gen_consensus(B, n_consensus, max_tries=10, modularity_fn=None):
         if n_uncertain == 0:
             break
 
-        B = A_cons - A_cons[*np.triu_indices_from(A_cons, k=1)].mean()
+        A = A_cons.copy()
 
     return comms, A_cons_init
